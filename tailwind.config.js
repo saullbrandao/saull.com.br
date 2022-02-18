80978896e88e9cdc68f73414b1b25f17d3ea25ca
Saull Brandão <saull@outlook.com>
2022-02-18 08:08:43 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  darkMode: "media",
+  theme: {
+    colors: {
+      blue: "#3689e6",
+      gray: {
+        200: "#d4d4d4",
+        800: "#333333",
+      },
+      black: "#1a1a1a",
+      white: "#ffffff",
+    },
+    fontFamily: {
+      sans: [
+        "Inter",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Oxygen",
+        "Ubuntu",
+        "Cantarell",
+        "Open Sans",
+        "Helvetica Neue",
+        "sans-serif",
+      ],
+    },
+  },
+  variants: {},
+  plugins: [],
+};
