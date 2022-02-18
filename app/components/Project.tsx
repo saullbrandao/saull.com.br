5b4399354dc6b4cca39ffd08ab7351df2ebd2c05
Saull Brandão <saull@outlook.com>
2022-02-18 10:19:18 -0300

    feat: portfolio page

---
+++ app/components/Project.tsx
@@ -0,0 +1,59 @@
+import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa'
+
+interface ProjectProps {
+  title: string
+  url: string
+  repo: string
+  description: {
+    enUS: string[]
+  }
+  images: {
+    light: string
+    dark: string
+  }
+}
+
+export const Project = ({
+  title,
+  url,
+  description,
+  repo,
+  images
+}: ProjectProps) => {
+  return (
+    <div className="flex flex-col gap-6 rounded-xl bg-gray-200 p-9 dark:bg-gray-800">
+      <div className="flex items-center justify-center gap-6 text-2xl font-bold text-black dark:text-white">
+        <h2>{title}</h2>
+        <a
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+          className="hover:text-blue"
+        >
+          <FaExternalLinkAlt />
+        </a>
+        <a
+          href={repo}
+          target="_blank"
+          rel="noreferrer"
+          className="hover:text-blue"
+          aria-label="Github Repository"
+        >
+          <FaGithub />
+        </a>
+      </div>
+      <img
+        className="hidden sm:block"
+        src={images.light}
+        alt="demo"
+        width={1920}
+        height={930}
+      />
+      <ul className="list-inside list-[circle] leading-8 text-gray-800 dark:text-gray-200">
+        {description['enUS'].map((el, index) => (
+          <li key={index}>{el}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
---
+++ app/routes/portfolio.tsx
@@ -0,0 +1,36 @@
+import { Project } from '~/components/Project'
+import projectsList from '../../projects.json'
+
+export default function Portfolio() {
+  return (
+    <div className="flex w-full flex-col items-center justify-center dark:bg-black">
+      <nav className="sticky top-0 z-10 flex w-full justify-center bg-gray-200 p-4 text-black dark:bg-gray-800 dark:text-white">
+        <div className="w-5xl flex w-full max-w-5xl justify-between">
+          <a className="text-xl font-bold hover:text-blue" href="/">
+            Saull Brandão
+          </a>
+        </div>
+      </nav>
+      <div className="mt-4 flex max-w-5xl flex-col items-center gap-12">
+        <h1 className="self-start text-3xl font-bold text-blue">Projects</h1>
+        {projectsList.map((project) => {
+          return (
+            <Project
+              description={project.description}
+              repo={project.repo}
+              title={project.title}
+              url={project.url}
+              key={project.id}
+              images={project.images}
+            />
+          )
+        })}
+        <footer className="p-6 text-gray-800">
+          <span>
+            © {new Date().getFullYear()} Saull Brandão. All rights reserved.
+          </span>
+        </footer>
+      </div>
+    </div>
+  )
+}
