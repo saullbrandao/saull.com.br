2aab32cd0143f96174a732c72d0a76863b434ea4
Saull Brandão <saull@outlook.com>
2022-02-23 19:46:04 -0300

    fix: resume route

--- app/routes/resume[.]pdf.ts
+++ app/routes/resume[.]pdf.ts
@@ -1,13 +1,7 @@
 import { LoaderFunction } from 'remix'
-import fs from 'fs'
 
 export const loader: LoaderFunction = async () => {
-  const pdf = fs.readFileSync('resume.pdf')
-
-  return new Response(pdf, {
-    headers: {
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': 'inline; filename=resume.pdf'
-    }
-  })
+  return fetch(
+    'https://github.com/saullbrandao/saull.com.br/raw/main/resume.pdf'
+  )
 }
