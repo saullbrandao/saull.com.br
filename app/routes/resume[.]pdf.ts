ef35d443be7171d97507071a3fc2406abdfef1c3
Saull Brandão <saull@outlook.com>
2022-02-23 19:29:42 -0300

    fix: resume route

--- app/routes/resume[.]pdf.ts
+++ app/routes/resume[.]pdf.ts
@@ -1,35 +1,9 @@
-import { google } from 'googleapis'
 import { LoaderFunction } from 'remix'
-import MemoryStream from 'memorystream'
-
-const drive = google.drive('v3')
+import fs from 'fs'
 
 export const loader: LoaderFunction = async () => {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: __dirname + '../../credentials.json',
-    scopes: 'https://www.googleapis.com/auth/drive.readonly'
-  })
+  const pdf = fs.readFileSync('resume.pdf')
 
-  google.options({ auth })
-
-  const fileId = '1IhUePfVCZ9W93sWr9-NG7m3U6jKgBntyuXxpucKkQWI'
-
-  const res = await drive.files.export(
-    { fileId, mimeType: 'application/pdf' },
-    { responseType: 'stream' }
-  )
-
-  const pdf = new MemoryStream()
-
-  await new Promise((resolve, reject) => {
-    res.data
-      .on('error', reject)
-      .pipe(pdf)
-      .on('error', reject)
-      .on('finish', resolve)
-  })
-
-  // @ts-ignore
   return new Response(pdf, {
     headers: {
       'Content-Type': 'application/pdf',
