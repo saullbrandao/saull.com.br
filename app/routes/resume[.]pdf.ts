020ee5cc330b5bb0d6b2807ae08894ee27804cfc
Saull Brandão <saull@outlook.com>
2022-02-23 19:48:15 -0300

    feat: display the resume on the page

--- app/routes/resume[.]pdf.ts
+++ app/routes/resume[.]pdf.ts
@@ -1,7 +1,15 @@
 import { LoaderFunction } from 'remix'
 
 export const loader: LoaderFunction = async () => {
-  return fetch(
+  const res = await fetch(
     'https://github.com/saullbrandao/saull.com.br/raw/main/resume.pdf'
   )
+
+  // @ts-ignore
+  return new Response(res.body, {
+    headers: {
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'inline; filename=resume.pdf'
+    }
+  })
 }
