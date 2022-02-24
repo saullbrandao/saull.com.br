7537c4e0026fe00f2debcca183d6000a69865260
Saull Brandão <saull@outlook.com>
2022-02-24 08:24:55 -0300

    feat: error handling on resume route

--- app/routes/resume[.]pdf.ts
+++ app/routes/resume[.]pdf.ts
@@ -1,15 +1,19 @@
 import { LoaderFunction } from 'remix'
 
 export const loader: LoaderFunction = async () => {
-  const res = await fetch(
-    'https://github.com/saullbrandao/saull.com.br/raw/main/resume.pdf'
-  )
+  try {
+    const res = await fetch(
+      'https://github.com/saullbrandao/saull.com.br/raw/main/resume.pdf'
+    )
 
-  // @ts-ignore
-  return new Response(res.body, {
-    headers: {
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': 'inline; filename=resume.pdf'
-    }
-  })
+    // @ts-ignore
+    return new Response(res.body, {
+      headers: {
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': 'inline; filename=resume.pdf'
+      }
+    })
+  } catch {
+    throw new Response('Error loading resume. Try again later', { status: 400 })
+  }
 }
